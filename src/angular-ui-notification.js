95e28a886a66a47a9dc8f16529b76ca670e64319
Nikita Lavrenko <lavrenonik@gmail.com>
2018-08-14 20:18:28 +0300

    dynamic maxCount fix
    Fix dynamic change maxCount option.

--- src/angular-ui-notification.js
+++ src/angular-ui-notification.js
@@ -26,6 +26,7 @@ angular.module('ui-notification').provider('Notification', function() {
 
     this.$get = function($timeout, $http, $compile, $templateCache, $rootScope, $injector, $sce, $q, $window) {
         var options = this.options;
+        var self = this;
 
         var startTop = options.startTop;
         var startRight = options.startRight;
@@ -130,7 +131,7 @@ angular.module('ui-notification').provider('Notification', function() {
 
                         lastPosition[element._positionY+element._positionX] = top + elHeight;
 
-                        if (options.maxCount > 0 && messageElements.length > options.maxCount && i === 0) {
+                        if (self.options.maxCount > 0 && messageElements.length > self.options.maxCount && i === 0) {
                             element.scope().kill(true);
                         }
 
